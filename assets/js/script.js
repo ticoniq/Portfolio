2eba0221a8cd3e4e4e7023607ddbec8387a4d62f
Henok Kirubel <henokhm2@gmail.com>
2023-05-15 16:04:16 +0300

    Fix ESLint errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
-const open_menu = document.querySelector("#open-menu");
-const close_menu = document.querySelector("#close-menu");
-const mobile_nav = document.querySelector("#mobile-nav");
-const blur_bg = document.querySelector("#blur-bg");
-const mobile_nav_link = document.querySelectorAll(".mobile-nav-link");
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const mobileNav = document.querySelector('#mobile-nav');
+const blurBg = document.querySelector('#blur-bg');
+const mobileNavLink = document.querySelectorAll('.mobile-nav-link');
 
 // Open mobile nav
-open_menu.addEventListener("click", () => {
-  mobile_nav.style.display = "flex";
-  blur_bg.style.display = "block";
-  document.body.style.overflow = "hidden";
+openMenu.addEventListener('click', () => {
+  mobileNav.style.display = 'flex';
+  blurBg.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 });
 
 const handleCloseMobileNav = () => {
-  mobile_nav.style.display = "none";
-  blur_bg.style.display = "none";
-  document.body.style.overflow = "auto";
+  mobileNav.style.display = 'none';
+  blurBg.style.display = 'none';
+  document.body.style.overflow = 'auto';
 };
 
 // Handle clicking the close mobile menu icon
-close_menu.addEventListener("click", handleCloseMobileNav);
+closeMenu.addEventListener('click', handleCloseMobileNav);
 
 // Handle clicking a mobile menu link
-mobile_nav_link.forEach((link) =>
-  link.addEventListener("click", handleCloseMobileNav)
-);
+mobileNavLink.forEach((link) => {
+  link.addEventListener('click', handleCloseMobileNav);
+});
 
 // Hide mobile navbar and blurred div
 // for desktop on window resize
