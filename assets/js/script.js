983e939e998478788a6468cb3d048c6bb47dfd24
Henok Kirubel <henokhm2@gmail.com>
2023-05-15 15:17:53 +0300

    Hide mobile navbar and blurred div on desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,21 @@ open_menu.addEventListener("click", () => {
   mobile_nav.style.display = "flex";
   blur_bg.style.display = "block";
   document.body.style.overflow = "hidden";
-  
 });
 
-// Close mobile nav
-close_menu.addEventListener("click", () => {
+const handleCloseMobileNav = () => {
   mobile_nav.style.display = "none";
   blur_bg.style.display = "none";
   document.body.style.overflow = "auto";
-});
+};
+
+// Close mobile nav
+close_menu.addEventListener("click", handleCloseMobileNav);
+
+// Hide mobile navbar and blurred div
+// for desktop on window resize
+window.onresize = () => {
+  if (window.innerWidth >= 768) {
+    handleCloseMobileNav();
+  }
+};
