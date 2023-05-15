9ecf956649655314a1724fbe97055a530a07d0d3
Henok Kirubel <henokhm2@gmail.com>
2023-05-15 13:05:23 +0300

    Add mobile menu styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 const open_menu = document.querySelector("#open-menu");
 const close_menu = document.querySelector("#close-menu");
 const mobile_nav = document.querySelector("#mobile-nav");
+const blur_bg = document.querySelector("#blur-bg");
 
 // Open mobile nav
-open_menu.addEventListener('click', () => {
-  mobile_nav.style.display = "block";
+open_menu.addEventListener("click", () => {
+  mobile_nav.style.display = "flex";
+  blur_bg.style.display = "block";
 });
 
-// Close mobile nav 
+// Close mobile nav
 close_menu.addEventListener("click", () => {
   mobile_nav.style.display = "none";
+  blur_bg.style.display = "none";
 });
