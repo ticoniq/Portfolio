345f1f68c5d00fc3da756b87f00beda098c898a3
Henok Kirubel <henokhm2@gmail.com>
2023-05-16 18:17:55 +0300

    Add script to generate project modal using JS

--- assets/js/generate-html-utils.js
+++ assets/js/generate-html-utils.js
@@ -38,7 +38,7 @@ const generateProjectCard = (projectDetails) => {
     <ul class="tags">
       ${generateTechnologyTags(technologies)}
     </ul>
-    <button class="btn btn-outline btn-hover mt-15 btn-pressed" onClick="() => nandleProjectCardClick(${id})">
+    <button id="open-project-popup-${id}" class="btn btn-outline btn-hover mt-15 btn-pressed">
       See Project
     </button>
   </div>
@@ -54,6 +54,53 @@ export const generateProjectCards = (projectsData) => {
 
 // TODO: update the following function
 export const generateProjectPopUps = (projectDetails) => {
-  projectDetails = 4;
-  return projectDetails;
+  const {
+    name,
+    role,
+    platform,
+    year,
+    description,
+    featuredImage,
+    technologies,
+    linkToLiveVersion,
+    linkToSource,
+  } = projectDetails;
+
+  return `
+    <div class="container">
+      <div class="card">
+        <div class="card-body">
+          <h2>${name}</h2>
+          <ul class="details">
+            <li>
+              <a href="#">${role}<i class="fa fa-circle"></i></a>
+            </li>
+            <li>
+              <a href="#">${platform}<i class="fa fa-circle"></i></a>
+            </li>
+            <li><a href="#">${year}</a></li>
+          </ul>
+          <img src="${featuredImage}" alt="" />
+        </div>
+        <div class="grid">
+          <p>
+            ${description}
+          </p>
+          <div class="btnn">
+            <ul class="tags">
+               ${generateTechnologyTags(technologies)}
+            </ul>
+            <div class="btn-group">
+              <a href="${linkToLiveVersion}" class="btn btn-outline">
+                See live <i class="fa fa-circle"></i>
+              </a>
+              <a href="${linkToSource}" class="btn btn-outline">
+                See source <i class="fab fa-github-alt"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+`;
 };
