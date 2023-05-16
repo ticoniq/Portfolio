44bc88263805ac8e98ee2a263ca770b03daff7ac
Henok Kirubel <henokhm2@gmail.com>
2023-05-16 11:11:52 +0300

    Add projects data to js file

---
+++ assets/js/projects-data.js
@@ -0,0 +1,56 @@
+const projectsData = [
+  {
+    id: '01',
+    name: 'Tonia',
+    role: 'Developer',
+    projectPlatform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'assets/images/portfolio-1.svg',
+    technologies: ['HTML', 'CSS', 'ReactJs'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '02',
+    name: 'Tonia',
+    role: 'Developer',
+    projectPlatform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'assets/images/portfolio-2.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Ruby on rails'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '03',
+    name: 'Tonia',
+    role: 'Developer',
+    projectPlatform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'assets/images/portfolio-3.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+  {
+    id: '04',
+    name: 'Tonia',
+    role: 'Developer',
+    projectPlatform: 'Web',
+    year: '2022',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: 'assets/images/portfolio-4.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    linkToLiveVersion: '#',
+    linkToSource: '#',
+  },
+];
+
+export default projectsData;
